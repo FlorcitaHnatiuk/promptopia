b9b7ff3ff7100a5ae1b20f701180a5e5e41f0c20
Florcita Hnatiuk <flopi.hnatiuk@gmail.com>
2023-08-08 14:59:37 -0300

    mongoose connection + OAuth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,16 +4,24 @@ import GoogleProvider from 'next-auth/providers/google';
 const handler = NextAuth({
     providers: [
         GoogleProvider({
-            clientId: '',
-            clientSecret: '',
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
     callbacks: {
         async session({ session }) {
-
+            try {
+                
+            } catch (error) {
+                
+            }
         },
         async signIn({ profile }) {
-            
+            try {
+                // serverless route => lambda function => dynamodb - it only opens up when it is called  
+            } catch (error) {
+                
+            }
         },
     }
 })

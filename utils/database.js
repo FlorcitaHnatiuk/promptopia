b64eb2da2bee852344d985378d81b23d74701964
Florcita Hnatiuk <flopi.hnatiuk@gmail.com>
2023-08-11 15:46:59 -0300

    connected to mongodb

--- utils/database.js
+++ utils/database.js
@@ -1,26 +1,23 @@
-import mongoose from 'mongoose';
-
-let isConnected = false; // track the connection
-
-export const connectToDB = async () => {
-    mongoose.set('strictQuery', true);
-
-    if (isConnected) {
-        console.log('MongoDB is already connected');
-        return;
-    }
-
-    try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            dbName: "share_prompt",
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
-
-        isConnected = true;
-
-        console.log('MongoDB connected')
-    } catch (error) {
-        console.log(error);
-    }
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://flor:emma3012@cluster0.qkh6c1w.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
 }
+run().catch(console.dir);
